802811f9a91c7156581ddd5e1de3ad9517b14b13
Ken Dale <ken@kendaleiv.com>
2014-02-21 13:25:06 -0500

    Add ExtraInitializationTests

---
+++ Tests/ExtraInitializationTests.cs
@@ -0,0 +1,49 @@
+﻿using Core;
+using StructureMap;
+using Xunit;
+
+namespace Tests
+{
+    public class ExtraInitializationTests
+    {
+        [Fact]
+        public void OnCreation()
+        {
+            var ran = false;
+
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<IService>().Use<Service>().OnCreation((context, myService) =>
+                {
+                    // You could run a method on service in here, etc.
+                    // For testing, we'll just modify the ran variable.
+
+                    ran = true;
+                });
+            });
+
+            var service = ObjectFactory.GetInstance<IService>();
+
+            Assert.True(ran);
+        }
+
+        
+        [Fact]
+        public void SettingPropertyDuringInitialization()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<TypeWithSetter>().Use<TypeWithSetter>().WithProperty("Id").EqualTo("value");
+            });
+
+            var service = ObjectFactory.GetInstance<TypeWithSetter>();
+
+            Assert.Equal("value", service.Id);
+        }
+
+        public class TypeWithSetter
+        {
+            public string Id { get; set; }
+        }
+    }
+}
