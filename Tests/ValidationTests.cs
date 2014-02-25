6d0d7366d2765443206b3bc7627908075f69feac
Ken Dale <ken@kendaleiv.com>
2014-02-25 08:56:38 -0500

    Add AssertConfigurationIsValid tests

---
+++ Tests/ValidationTests.cs
@@ -0,0 +1,45 @@
+﻿using Core;
+using StructureMap;
+using StructureMap.Exceptions;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void VerifyValidConfiguration()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<IService>().Use<Service>();
+            });
+
+            ObjectFactory.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void VerifyInvalidConfiguration()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<IService>().Use<BrokenService>();
+            });
+
+            Assert.Throws<StructureMapConfigurationException>(
+                () => ObjectFactory.AssertConfigurationIsValid());
+        }
+
+        public class BrokenService : IService
+        {
+            public string Id { get; private set; }
+
+            [ValidationMethod]
+            public void BrokenMethod()
+            {
+                throw new ApplicationException();
+            }
+        }
+    }
+}
