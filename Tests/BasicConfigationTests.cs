dc7c7aa7d155b95526e019377a1e7dc52395bf52
Ken Dale <ken@kendaleiv.com>
2014-02-25 08:48:15 -0500

    Add multiple named instances on same type test

--- Tests/BasicConfigationTests.cs
+++ Tests/BasicConfigationTests.cs
@@ -21,6 +21,20 @@ namespace Tests
             Assert.IsType<Service>(service);
         }
 
+        [Fact]
+        public void GetInstanceT_MultipleNamedInstances_LastInWins()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<IService>().Use<Service>().Named("A");
+                x.For<IService>().Use<ServiceB>().Named("B");
+            });
+
+            var service = ObjectFactory.GetInstance<IService>();
+
+            Assert.IsType<ServiceB>(service);
+        }
+
         /// <remarks>
         /// This isn't the best practice.
         /// Demoing that specifying constructor parameters as part of wireup is possible.
