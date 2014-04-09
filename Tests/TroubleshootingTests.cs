0699cb6e6895bbc8099c49641e49209a4c3b75c3
Ken Dale <ken@kendaleiv.com>
2014-04-09 08:09:58 -0400

    Add TroubleshootingTests ShowBuildPlan

--- Tests/TroubleshootingTests.cs
+++ Tests/TroubleshootingTests.cs
@@ -8,6 +8,27 @@ namespace Tests
     public class TroubleshootingTests
     {
         [Fact]
+        public void ShowBuildPlan()
+        {
+            var container = new Container(x =>
+            {
+                x.For<IService>().Use<Service>();
+            });
+
+            var buildPlan = container.Model.For<IService>()
+                .Default
+                .DescribeBuildPlan();
+
+            var expectedBuildPlan =
+@"PluginType: Core.IService
+Lifecycle: Transient
+new Service()
+";
+
+            Assert.Equal(expectedBuildPlan, buildPlan);
+        }
+
+        [Fact]
         public void WhatDoIHave()
         {
             var container = new Container(x =>
